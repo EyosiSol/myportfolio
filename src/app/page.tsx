f652ae86941e31eb9884f40af5ae3fc130c90440
EyosiSol <115923891+EyosiSol@users.noreply.github.com>
2024-12-19 11:24:14 +0300

    add My Services section on the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,10 +24,10 @@ const inter = Inter({
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center gap-10">
+    <div className="flex flex-col items-center justify-center gap-48">
       <div className="w-fit h-fit p-20 gap-4 flex flex-col items-center justify-center left-0 top-0  bg-[url('/images/Pattern.png')] bg-cover bg-center rounded-[500px]">
         <Image
-          className="w-[202px] h-[197px] left-[421px] top-[137px]  border border-red-500 rounded-full"
+          className="w-[202px] h-[197px] left-[421px] top-[137px] rounded-full"
           src="/profile.png"
           alt="profile"
           width={202}
@@ -74,31 +74,143 @@ export default function Home() {
         <div
           className={`w-[840px] left-0 top-[111px]  text-center text-[#e1e1e1] text-[17px] font-medium ${inter.className} font-inter leading-[25px]`}
         >
-          Hi there! I&apos;m [Your Name], a data scientist specializing in data
-          analytics, predictive modeling, natural language processing, machine
-          learning, and AI chatbots. With a passion for unraveling insights from
-          complex datasets, I&apos;m dedicated to helping businesses make
-          informed decisions and stay ahead in today&apos;s data-driven world.
-          <br />
-          <br />I bring a blend of technical expertise, hands-on experience, and
-          a commitment to clear communication to every project. Whether
-          it&apos;s uncovering hidden patterns, predicting future trends, or
-          automating processes with AI, I&apos;m here to help you harness the
-          full potential of your data.
-          <br />
-          <br />
-          Let&apos;s work together to transform your data into actionable
-          insights that drive real results. Get in touch, and let&apos;s start
-          unlocking the power of your data today!
+          Hi there! I&apos;m Eyosias Solomon, a passionate Graphics and UI/UX
+          Designer, React Developer, and Electronics Engineer. My expertise
+          spans dynamic web applications, intuitive UI/UX designs, and
+          innovative solutions that bridge technical precision and creative
+          design. Whether crafting visually engaging interfaces or developing
+          user-friendly front-end solutions, I&apos;m committed to delivering
+          seamless and impactful experiences. I bring a robust background in
+          Electrical and Computer Engineering combined with hands-on experience
+          in web development, leveraging technologies like React and Next.js. My
+          skill set includes a mastery of design tools like Figma and Adobe
+          Illustrator, ensuring every project is aesthetically pleasing and
+          functionally effective. From developing AI-driven chatbots to crafting
+          recipe-finding apps and microprocessor simulations, I thrive on
+          turning ideas into reality. Let&apos;s collaborate to create
+          exceptional digital experiences that resonate with users and achieve
+          your goals. Get in touch today to bring your vision to life!
         </div>
         <div className="w-[216px] h-[55px] p-2.5 left-[340px] top-[426px]  rounded-[55px] border border-[#4fc3f7] justify-center items-center gap-2.5 inline-flex hover:bg-[#4fc3f7] hover:text-white transition-all duration-300 cursor-pointer">
-          <div
-            className={`text-white text-[15px] font-medium ${montserrat.className} font-montserrat`}
+          <a
+            href="/pdf/Resume.pdf" // Correct path
+            download="my_Resume.pdf" // Suggested filename
+            target="_blank"
           >
-            Download Resume
+            <div
+              className={`text-white text-[15px] font-medium ${montserrat.className} font-montserrat`}
+            >
+              Download Resume
+            </div>
+          </a>
+        </div>
+      </div>
+      <div className="w-[929px] h-[691px] flex-col justify-start items-center gap-9 inline-flex">
+        <div className="flex-col justify-start items-center flex">
+          <div className="text-white text-[45px] font-extrabold font-['Inter']">
+            What I do
+          </div>
+          <div className="text-[#4fc3f7] text-sm font-semibold font-['Inter']">
+            My Services
+          </div>
+        </div>
+        <div className="self-stretch justify-start items-center h-full py-4 gap-5 inline-flex">
+          <div className="w-[620px] flex justify-center items-center gap-1 h-fit py-4 bg-gradient-to-br from-[#1b1b1b] to-[#050505] rounded-[10px]">
+            <div className="w-[300px] left-[20px] top-[101px] flex-col justify-start items-start gap-4 inline-flex">
+              <div className="left-[20px] top-[38px] justify-start items-start gap-2.5 inline-flex">
+                <div className="w-11 h-11 bg-[#f5f8ff] rounded-[9px]" />
+                <div className="w-6 h-6 overflow-hidden" />
+              </div>
+              <div className="text-white text-xl font-semibold font-['Inter'] leading-normal">
+                Front End Web development
+              </div>
+              <div className="w-[314px] text-[#e1e1e1] text-sm font-normal font-['Inter'] leading-[21px]">
+                Creating user interface and experience of a website application
+                using technologies like HTML, CSS, and JavaScript, ensuring
+                functionality, responsiveness, and aesthetic appeal.
+              </div>
+            </div>
+            <Image
+              className="rounded-[10px]"
+              src="/images/webdev2.jpg"
+              alt="Web Development"
+              width={280}
+              height={280}
+            />
+          </div>
+          <div className="w-[289px] h-auto flex justify-center items-center bg-gradient-to-br from-[#1b1b1b] to-[#050505] rounded-[10px]">
+            <div className="w-[289px] py-2 h-full flex-col gap-6 justify-start items-start pl-[26px] inline-flex">
+              <div className="p-2.5 bg-[#f5f8ff] rounded-[9px] justify-start items-start gap-2.5 inline-flex overflow-hidden">
+                <div className="w-6 h-6 overflow-hidden" />
+              </div>
+              <div className="h-[157px] flex-col justify-start items-start gap-[18px] inline-flex">
+                <div className="w-[237px] text-white text-[15px] font-semibold font-['Inter'] leading-[18px]">
+                  Ui/Ux Design
+                </div>
+                <div className="w-[243px] h-[121px] text-[#e1e1e1] text-sm font-normal font-['Inter'] leading-[21px]">
+                  Specializing in crafting intuitive, user-friendly interfaces
+                  and seamless digital experiences through innovative and
+                  thoughtful UI/UX design solutions.
+                  <br />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="self-stretch justify-start items-start gap-5 inline-flex">
+          <div className="grow shrink basis-0 self-stretch pl-[26px] pr-3 pt-[29px] pb-[25px] flex-col justify-end items-start gap-[19px] inline-flex bg-gradient-to-br from-[#1b1b1b] to-[#050505] rounded-[10px]">
+            <div className="p-2.5 bg-[#f5f8ff] rounded-[9px] justify-start items-start gap-2.5 inline-flex overflow-hidden">
+              <div className="w-6 h-6 overflow-hidden" />
+            </div>
+            <div className="self-stretch h-[157px] flex-col justify-start items-start gap-[18px] inline-flex">
+              <div className="w-[237px] text-white text-[15px] font-semibold font-['Inter'] leading-[18px]">
+                {" "}
+                Product Design
+              </div>
+              <div className="w-[243px] h-[121px] text-[#e1e1e1] text-sm font-normal font-['Inter'] leading-[21px]">
+                Designing innovative, user-focused digital products that
+                seamlessly combine functionality, aesthetics, and usability to
+                deliver exceptional experiences.
+                <br />
+              </div>
+            </div>
+          </div>
+          <div className="grow shrink basis-0 self-stretch pl-[26px] pr-[7px] pt-[29px] pb-1.5 flex-col justify-end items-start gap-[19px] inline-flex bg-gradient-to-br from-[#1b1b1b] to-[#050505] rounded-[10px]">
+            <div className="p-2.5 bg-[#f5f8ff] rounded-[9px] justify-start items-start gap-2.5 inline-flex overflow-hidden">
+              <div className="w-6 h-6 overflow-hidden" />
+            </div>
+            <div className="self-stretch h-[175px] flex-col justify-start items-start gap-[18px] inline-flex">
+              <div className="w-[237px] text-white text-[15px] font-semibold font-['Inter'] leading-[18px]">
+                PCB Design
+              </div>
+              <div className="w-[270px] h-[121px] text-[#e1e1e1] text-sm font-normal font-['Inter'] leading-[21px]">
+                Designing efficient, high-performance PCBs with precision,
+                ensuring optimal functionality and reliability for diverse
+                electronic applications and innovative solutions
+                <br />
+              </div>
+            </div>
+          </div>
+          <div className="grow shrink basis-0 self-stretch pl-[26px] pr-3 pt-[29px] pb-6 flex-col justify-end items-start gap-[19px] inline-flex bg-gradient-to-br from-[#1b1b1b] to-[#050505] rounded-[10px]">
+            <div className="p-2.5 bg-[#f5f8ff] rounded-[9px] justify-start items-start gap-2.5 inline-flex overflow-hidden">
+              <div className="w-6 h-6 overflow-hidden" />
+            </div>
+            <div className="self-stretch h-[157px] flex-col justify-start items-start gap-[18px] inline-flex">
+              <div className="w-[237px] text-white text-[15px] font-semibold font-['Inter'] leading-[18px]">
+                Graphics Design
+              </div>
+              <div className="w-[243px] h-[121px] text-[#e1e1e1] text-sm font-normal font-['Inter'] leading-[21px]">
+                creating visually compelling content using typography, images,
+                colors, and layouts to communicate messages effectively across
+                various mediums.
+                <br />
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+// -- border border-[#2a2a2a]
