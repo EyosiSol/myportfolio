ea2e30646be6daa6c3070bb667c26e4203b0ceac
EyosiSol <115923891+EyosiSol@users.noreply.github.com>
2024-12-28 22:18:12 +0300

    fix: height issues

--- src/app/components/whatIdo.tsx
+++ src/app/components/whatIdo.tsx
@@ -25,7 +25,7 @@ export default function whatido({
       </div>
       <div className="grid md:grid-cols-3 items-center h-full py-4 gap-5 w-full">
         <div className=" w-full md:w-fit md:col-span-2 flex justify-center items-center gap-1 h-fit py-4 bg-gradient-to-br from-[#1b1b1b] to-[#050505] rounded-[10px]">
-          <div className="w-full md:w-fit px-3 md:px-0 left-[20px] top-[101px] flex-col justify-center md:justify-start md:items-start gap-4 inline-flex">
+          <div className="w-full md:w-fit px-3 md:px-3 left-[20px] top-[101px] flex-col justify-center md:justify-start md:items-start gap-4 inline-flex">
             <div className="hidden md:flex left-[20px] top-[38px] justify-center md:justify-start md:items-start gap-2.5">
               <div className="w-11 h-11 bg-[#f5f8ff] rounded-[9px]" />
               <div className="w-6 h-6 overflow-hidden" />
@@ -52,8 +52,8 @@ export default function whatido({
           />
         </div>
         <div className="w-full h-full flex flex-col   bg-gradient-to-br from-[#1b1b1b] to-[#050505] rounded-[10px]">
-          <div className="w-full md:w-full md:h-full flex-col gap-3 justify-center items-center md:items-start pl-3 md:pl-[26px] inline-fle">
-            <div className=" hidden md:block p-2 bg-[#f5f8ff] rounded-[9px] md:gap-2.5  overflow-hidden">
+          <div className="w-full md:w-full md:h-full flex-col gap-3 justify-center items-center md:items-start pl-3 md:pl-[26px] inline-flex">
+            <div className="hidden md:block p-2 bg-[#f5f8ff] rounded-[9px] md:gap-2.5  overflow-hidden">
               <div className="w-6 h-6 overflow-hidden" />
             </div>
             <div className=" h-full py-4 md:h-[157px] flex-col justify-start items-start gap-[18px] inline-flex">
@@ -74,11 +74,11 @@ export default function whatido({
         </div>
       </div>
       <div className="w-full flex-col md:flex-row justify-start items-start gap-5 flex ">
-        <div className="md:p-6 flex-col justify-center items-center md:justify-end md:items-start md:gap-[19px] inline-flex bg-gradient-to-br from-[#1b1b1b] to-[#050505] rounded-[10px] pl-3">
+        <div className="md:p-6 flex-col justify-center items-center md:justify-end md:items-start md:gap-[19px] inline-flex bg-gradient-to-br from-[#1b1b1b] to-[#050505] rounded-[10px] pl-3 ">
           <div className=" hidden md:flex p-2.5 bg-[#f5f8ff] rounded-[9px] justify-start items-start gap-2.5  overflow-hidden">
             <div className="w-6 h-6 overflow-hidden" />
           </div>
-          <div className="self-stretch py-4  h-auto md:h-auto flex-col justify-start items-start gap-[18px] inline-flex">
+          <div className="self-stretch py-4  h-auto md:h-full flex-col justify-start items-start gap-[18px] inline-flex">
             <div
               className={` w-full md:w-full text-white  text-xl  text-[15px] font-semibold ${inter.className} leading-[18px]`}
             >
@@ -86,7 +86,7 @@ export default function whatido({
               Product Design
             </div>
             <div
-              className={`w-full pr-1 md:w-auto h-auto md:h-[121px] text-[#e1e1e1] text-sm font-normal ${inter.className} leading-[21px]`}
+              className={`w-full pr-1 md:w-auto h-auto md:h-full text-[#e1e1e1] text-sm font-normal ${inter.className} leading-[21px]`}
             >
               Designing innovative, user-focused digital products that
               seamlessly combine functionality, aesthetics, and usability to
@@ -95,19 +95,19 @@ export default function whatido({
             </div>
           </div>
         </div>
-        <div className="md:p-6 flex-col justify-center items-center md:justify-end md:items-start md:gap-[19px] inline-flex bg-gradient-to-br from-[#1b1b1b] to-[#050505] rounded-[10px] pl-3">
+        <div className="md:p-6 flex-col justify-center items-center md:justify-end md:items-start md:gap-[19px] inline-flex bg-gradient-to-br from-[#1b1b1b] to-[#050505] rounded-[10px] pl-3 ">
           <div className=" hidden md:flex p-2.5 bg-[#f5f8ff] rounded-[9px] justify-start items-start gap-2.5  overflow-hidden">
             <div className="w-6 h-6 overflow-hidden" />
           </div>
-          <div className="self-stretch py-4  h-auto md:h-auto flex-col justify-start items-start gap-[18px] inline-flex">
+          <div className="self-stretch py-4  h-auto md:h-full flex-col justify-start items-start gap-[18px] inline-flex">
             <div
-              className={` w-full md:w-auto text-white  text-xl  text-[15px] font-semibold ${inter.className} leading-[18px]`}
+              className={`w-full md:w-full text-white  text-xl  text-[15px] font-semibold ${inter.className} leading-[18px]`}
             >
               {" "}
               Graphics Design
             </div>
             <div
-              className={` w-full pr-1 md:w-auto h-auto md:h-auto text-[#e1e1e1] text-sm font-normal ${inter.className} leading-[21px]`}
+              className={`w-full pr-1 md:w-auto h-auto md:h-full text-[#e1e1e1] text-sm font-normal ${inter.className} leading-[21px]`}
             >
               creating visually compelling content using typography, images,
               colors, and layouts to communicate messages effectively across
@@ -116,7 +116,7 @@ export default function whatido({
             </div>
           </div>
         </div>
-        <div className="md:p-6 flex-col justify-center items-center md:justify-end md:items-start md:gap-[19px] inline-flex bg-gradient-to-br from-[#1b1b1b] to-[#050505] rounded-[10px]pl-3">
+        <div className="md:p-6 flex-col justify-center items-center md:justify-end md:items-start md:gap-[19px] inline-flex bg-gradient-to-br from-[#1b1b1b] to-[#050505] rounded-[10px] pl-3">
           <div className=" hidden md:flex p-2.5 bg-[#f5f8ff] rounded-[9px] justify-start items-start gap-2.5  overflow-hidden">
             <div className="w-6 h-6 overflow-hidden" />
           </div>
@@ -128,7 +128,7 @@ export default function whatido({
               PCB Design
             </div>
             <div
-              className={` w-full pr-1 md:w-auto h-auto md:h-auto text-[#e1e1e1] text-sm font-normal ${inter.className} leading-[21px]`}
+              className={` w-full pr-1 md:w-auto h-auto md:h-full text-[#e1e1e1] text-sm font-normal ${inter.className} leading-[21px]`}
             >
               Designing efficient, high-performance PCBs with precision,
               ensuring optimal functionality and reliability for diverse
